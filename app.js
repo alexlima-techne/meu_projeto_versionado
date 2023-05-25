d5a700074e66b30d5d4995f8887f3d261e447308
Alex Lima <alex.lima@techne.com.br>
2023-05-25 16:18:13 -0300

    feature 2

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ console.log("Commit 1");
 console.log("Commit 2");
 console.log("Commit 3");
 
-const meuConsole = (msg) => console.log(msg);
-meuConsole("feature 1");
+const meuConsole = (msg, csl) => cls.log(msg);
+meuConsole("feature 2",console);
